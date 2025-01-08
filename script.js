277fdf453c1b3a91f02ba12e3a19bdee48df0386
JashawnRogers <rogersjashawn20@gmail.com>
2025-01-07 20:31:47 -0700

    Implemented dark mode toggle functionality

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
+const darkModeBtn = document.querySelector('#dark-mode-btn');
+
 const toggleMenu = () => {
     const menu = document.querySelector('.menu-links');
     const icon = document.querySelector('.hamburger-icon');
     menu.classList.toggle('open');
     icon.classList.toggle('open');
-}
+};
+
+darkModeBtn.addEventListener('click', () => {
+    document.querySelectorAll('.icon').forEach(icon => {
+        if (icon.style.filter === 'invert(1)') {
+            icon.style.filter = '';
+        } else {
+            icon.style.filter = 'invert(1)';
+        }
+    });
+    document.body.classList.toggle('dark-mode');
+});
+
